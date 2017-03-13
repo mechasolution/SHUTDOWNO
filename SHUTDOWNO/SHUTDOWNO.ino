b8d99bdc5bc1e1dbd819aa98c5d6b80128d56479
김재균 <worbs0604@gmail.com>
2017-03-13 15:26:57 +0900

    Add files via upload

---
+++ SHUTDOWNO/SHUTDOWNO.ino
@@ -0,0 +1,30 @@
+#include <Keyboard.h>
+
+char val = 0;
+
+void setup() {
+  Keyboard.begin();
+  Serial1.begin(9600);
+  delay(5000);
+}
+
+void loop() {
+  if (Serial1.available()) {
+    switch (Serial1.read()) {
+      case '1' :
+        Keyboard.press(KEY_LEFT_GUI);
+        Keyboard.press('l');
+        Keyboard.releaseAll();
+        break ;
+      case '2' :
+        Keyboard.press(KEY_LEFT_GUI);
+        Keyboard.press('r');
+        Keyboard.releaseAll();
+        delay(300);
+        Keyboard.print("shutdown -s -t 0");
+        delay(300);
+        Keyboard.write(KEY_RETURN);
+        break ;
+    }
+  }
+}
